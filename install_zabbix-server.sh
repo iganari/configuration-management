b618fb8cf905f8f10be09100aa3c9f15a840279e
a-igaiga <igarashi.for.aucbiz@gmail.com>
2014-08-26 04:48:20 +0900

    update zabbix repo

--- install_zabbix-server.sh
+++ install_zabbix-server.sh
@@ -32,7 +32,7 @@ chkconfig mysqld on
 rpm -ivh http://repo.zabbix.com/zabbix/2.2/rhel/6/x86_64/zabbix-release-2.2-1.el6.noarch.rpm
 sed -i "s/enabled=1/enabled=0/g" /etc/yum.repos.d/zabbix.repo
 
-yum install --enablerepo=zabbix zabbix-server-mysql zabbix-web-mysql zabbix-web-japanese zabbix-agent zabbix-get zabbix-web-japanese -y
+yum install --enablerepo=zabbix --enablerepo=zabbix-non-supported zabbix-server-mysql zabbix-web-mysql zabbix-web-japanese zabbix-agent zabbix-get zabbix-web-japanese -y
 
 # setting mysql for zabbix
 mysql -uroot < ./source/zabbix_server.sql
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 #
 # created 2014/08/20
 
-/bin/bash -x auto_install-base.sh
+# /bin/bash -x auto_install-base.sh
 # /bin/bash -x auto_install-web_server.sh
