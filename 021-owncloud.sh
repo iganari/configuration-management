3ec85194eb1c3ff0015ea9211a306b6f8c9d6533
lassi <lassi@sun.(none)>
2014-11-01 12:30:14 +0900

    modify setting owncloud

--- 021-owncloud.sh
+++ 021-owncloud.sh
@@ -2,4 +2,16 @@
 #
 # created 2014/10/30
 
+# ####### NOTE #######
+# running sudo user !!
+# ####################
+
+
+cd /etc/yum.repos.d
+sudo wget http://download.opensuse.org/repositories/isv:ownCloud:community/CentOS_CentOS-6/isv:ownCloud:community.repo
+sudo yum install owncloud
+msql -uroot < ./source/owncloud.sql
+
+
+
 
---
+++ source/owncloud.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE owncloud;
+GRANT ALL PRIVILEGES ON owncloud.* to 'owncloud'@'localhost' IDENTIFIED BY 'owncloud';
