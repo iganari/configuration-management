bc78cf71ee29c8851fcd95d9fcaa4c5e383f7a7a
aucfan-igrashi <igarashi@aucfan.com>
2015-09-27 16:13:44 +0900

    update

---
+++ install_command-jq.sh
@@ -0,0 +1,11 @@
+#/bin/bah
+
+### permisson
+ check-permission.sh
+
+# source check-permission.sh
+
+echo "yahoo"
+# wget http://stedolan.github.io/jq/download/linux64/jq
+# chmod 755 jq
+# mv jq /usr/local/bin/
