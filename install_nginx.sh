21c0acc393a681c940ae5bb5d4c1acb4f0643558
aucfan-igarashi <cigaguri@gmail.com>
2015-10-06 23:59:51 +0900

    create install sehll of nginx

---
+++ install_nginx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+### get nginx repo rpm && registration
+rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+
+### install nginx from yum repo
+yum install nginx && service nginx start
+
+### nginx status check
+
+echo 'nginx status is'
+# curl -LI localhost -o /dev/null -w '%{http_code}\n' -s
+curl -LI localhost
+
