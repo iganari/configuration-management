3bdb0b0cc4b4d4e100a2adc8eec678c223c3c283
iganari <cigaguri@gmail.com>
2016-06-22 11:00:06 +0900

    add command of realtime enforce selinux

--- 0-server_base-install.sh
+++ 0-server_base-install.sh
@@ -79,6 +79,9 @@ echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 # modify SELinux
 sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
 
+### # force push when do not reboot
+### setenforce 0
+
 # bash color custom
 echo 'export PS1="\[\033[1;34m\][\t][\u@\h \W]\\$\[\033[0m\] "' >> /etc/profile 
 
